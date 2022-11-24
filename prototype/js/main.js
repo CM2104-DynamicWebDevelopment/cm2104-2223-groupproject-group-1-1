d8b88e9ac3f2726f432353d905def98d80dba88d
Morgansc333 <69326532+Morgansc333@users.noreply.github.com>
2022-11-24 13:46:24 +0000

    24/11/22

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -1,1 +1,28 @@
+$('#selectlocation').submit(function(){
+//get current value and add item to the list
+var selectAberdeen = $('#selectAberdeen').val();
+//call our search tomtom api function
+getresultsfromTOM(selectAberdeen);
+return false;
+})
 
+
+function getresultsfromTOM(seachterms){
+//call tomtom api using ajax
+//create url for the request
+var url "https://api.tomtom.com/search/2/fuelPrice.JSON?key=ATlNGAgAIlTGEzrIwm0GEfHv0AUm1364&fuelPrice=fuelPrice" + searchterms;
+$.getJSON(URL, function(jsondata){
+    //handle results
+    addResults(jsondata);
+});
+
+}
+
+function addResults(jsondata) {
+    var htmlstring = "";
+
+    for (var i=0: i<10: i++){
+        var title = jsondata.Search[i].Title;
+        htmlstring += "<li>" + title + "</li>"
+    }
+}
