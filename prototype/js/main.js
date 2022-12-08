263b742541b0e8fd47860cecb0a924bfbaad9aa1
morgansc3 <m.cresswell@rgu.ac.uk>
2022-12-08 13:10:10 +0000

    api

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -3,17 +3,31 @@ $('#selectlocation').submit(function(){
 var selectAberdeen = $('#selectAberdeen').val();
 //call our search tomtom api function
 getresultsfromTOM(selectAberdeen);
+console.log("here")
 return false;
 })
 
+getresultsfromTOM("test");
 
 function getresultsfromTOM(seachterms){
+var lat =51.5072;
+var lon =0.1276;    
 //call tomtom api using ajax
 //create url for the request
-var url "https://api.tomtom.com/search/2/fuelPrice.JSON?key=ATlNGAgAIlTGEzrIwm0GEfHv0AUm1364&fuelPrice=fuelPrice" + searchterms;
-$.getJSON(URL, function(jsondata){
+var url = "https://api.tomtom.com/search/2/categorySearch/electric%20vehicle%20station.json?lat="+lat+"&lon="+lon+"&extendedPostalCodesFor=POI&key=ATlNGAgAIlTGEzrIwm0GEfHv0AUm1364"
+$.getJSON(url, function(jsondata){
     //handle results
-    addResults(jsondata);
+    console.log(jsondata)
+
+    var listofpoints = jsondata.results;
+
+    listofpoints.forEach(function (point){
+        var url2 = "https://api.tomtom.com/search/2/chargingAvailability.json?chargingAvailability="+point.id+"&key=ATlNGAgAIlTGEzrIwm0GEfHv0AUm1364"
+        $.getJSON(url2, function(jsondata2){
+            console.log(jsondata2)
+        });
+    });
+   //addResults(jsondata);
 });
 
 }
