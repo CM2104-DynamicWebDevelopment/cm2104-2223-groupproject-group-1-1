51f19251540f17a440f68f87cfc525a95267ad34
PluckyAurora <romekbardzikzomb@gmail.com>
2023-04-12 16:20:48 +0100

    setting up server to read ejs files

---
+++ final/server.js
@@ -0,0 +1,32 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+ res.render('pages/index');
+});
+
+// Ev charge point page
+app.get('/EVchargePoints', function(req, res) {
+ res.render('pages/EVchargePoints');
+});
+
+// login page
+app.get('/login', function(req, res) {
+    res.render('pages/login');
+   });
+
+// create accounnt page
+app.get('/createAccount', function(req, res) {
+    res.render('pages/createAccount');
+   });
+
+app.listen(8080);
+console.log('8080 is the magic port');
